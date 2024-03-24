d622277f8000973025a1c2c2dc914f7d8d56c1a7
primboi <81822069+primitt@users.noreply.github.com>
2024-03-23 17:40:16 -0700

    Co-authored-by: Sebastian-Alexis <Sebastian-Alexis@users.noreply.github.com> Co-authored-by: HeckerWithAHat <HeckerWithAHat@users.noreply.github.com>

---
+++ utils/model.py
@@ -0,0 +1,19 @@
+from peewee import *
+import psycopg2
+
+database = PostgresqlDatabase('defaultdb', **{'host': 'pstdbdedi-do-user-11106940-0.c.db.ondigitalocean.com', 'port': 25060, 'user': 'doadmin', 'password': 'AVNS_4qIXT_v6ZdvCnbulAS_'})
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+class Dblk(BaseModel):
+    package = CharField(null=True)
+    subdeps = AnyField(null=True)
+
+    class Meta:
+        table_name = 'dblk'
+        primary_key = False
---
+++ utils/score.py
@@ -0,0 +1,80 @@
+import requests
+import time
+import json
+
+
+apikey_list = ["53d6ec55fcbfdedb9ac6bd44ae42050c", "10b8f9c2a81b273a6c0db61fb96fb212", "7165ca9cc733d1abd00a87a930d9d714"]
+
+
+apiKey = apikey_list[0]
+
+libname = "org.apache.pulsar:pulsar-functions-worker"
+libver = "2.11.3"
+pckmanager = "Maven"
+
+response = requests.get(f"https://libraries.io/api/maven/${libname}/${libver}/dependencies?api_key=${apiKey}")
+
+# Number of times to run the request
+num_requests = 1
+
+# Counter for rate limit errors
+rate_limit_errors = 0
+
+
+
+
+
+
+
+
+for _ in range(num_requests):
+  try:
+    data = {
+      "version": libver,
+      "package": {"name": libname, "ecosystem": pckmanager},
+    }
+
+    headers = {
+      "Content-Type": "application/json"
+    }
+
+    response = requests.post("https://api.osv.dev/v1/query", json=data, headers=headers)
+    response_json = response.json()
+    vulns = response_json.get("vulns", [])
+    vuln_data = {}
+
+    for vuln in vulns:
+        aliases = vuln.get("aliases", [])
+        details = vuln.get("details", "No details available")
+        affected_versions = vuln.get("affected", [{}])[0].get("versions", [])
+        fixed_version_events = vuln.get("affected", [{}])[0].get("ranges", [{}])[0].get("events", [])
+        fixed_version = next((event.get("fixed") for event in fixed_version_events if event.get("fixed")), "Not specified")
+        severity_info = vuln.get("severity", [])
+        
+        for severity in severity_info:
+            if severity.get("type") == "CVSS_V3":
+                score = severity.get("score")
+                cvss_score = score.split('/')[0] if score else None
+                break
+
+        for alias in aliases:
+            vuln_data[alias] = {
+                "CVSS Score": cvss_score,
+                "Details": details,
+                "Affected Versions": affected_versions,
+                "Fixed Version": fixed_version
+            }
+
+    print(vuln_data)
+
+
+
+
+    print(response.json())
+  except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+    break
+
+
+
+#GET https://libraries.io/api/search?q=grunt&api_key=YOUR_API_KEY
