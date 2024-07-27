f68801304424607e6151aadd73806c6216091fb4
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2024-07-27 15:23:41 -0700

    Update keys.py

--- utils/keys.py
+++ utils/keys.py
@@ -5,4 +5,4 @@
 
 keylist = ["53d6ec55fcbfdedb9ac6bd44ae42050c", "10b8f9c2a81b273a6c0db61fb96fb212", "7165ca9cc733d1abd00a87a930d9d714"]
 
-headers = {"Authorization":"Bearer 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"}
+headers = {"Authorization":"Bearer"}
